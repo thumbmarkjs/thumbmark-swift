a7f679e5dde41b9322d2e467517712e8ebb1412f
Brandon Stillitano <b.stillitano95@gmail.com>
2024-04-30 20:22:46 +1000

    Update example app

--- ExampleApp/Thumbmark Swift Example/ContentView.swift
+++ ExampleApp/Thumbmark Swift Example/ContentView.swift
@@ -9,79 +9,87 @@ import SwiftUI
 import Thumbmark
 
 struct ContentView: View {
+    @State var id: String = ""
+    @State var persistentId: String = ""
+    @State var fingerprint: Fingerprint?
+    
     var body: some View {
         NavigationStack {
             List {
                 Section {
-                    RowContent(title: "ID", subtitle: Thumbmark.instance.id)
-                    RowContent(title: "Persistent ID", subtitle: Thumbmark.instance.persistentId?.uuidString)
+                    RowContent(title: "ID", subtitle: id)
+                    RowContent(title: "Persistent ID", subtitle: persistentId)
                 }
                 Section {
-                    ForEach(Thumbmark.instance.fingerprint.captureDevices, id: \.id) { value in
+                    ForEach(fingerprint?.captureDevices ?? [], id: \.id) { value in
                         RowContent(title: value.name, subtitle: "ID: \(value.id)\nModel: \(value.modelId)\nPosition: \(value.position)")
                     }
                 } header: {
                     Text("Capture devices")
                 }
                 Section {
-                    RowContent(title: "Total cores", subtitle: Thumbmark.instance.fingerprint.processor.processorCount.description)
-                    RowContent(title: "Active cores", subtitle: Thumbmark.instance.fingerprint.processor.activeProcessors.description)
-                    RowContent(title: "Architecture", subtitle: Thumbmark.instance.fingerprint.processor.architecture)
-                    RowContent(title: "Kernel version", subtitle: Thumbmark.instance.fingerprint.processor.kernelVersion.description)
+                    RowContent(title: "Total cores", subtitle: fingerprint?.processor?.processorCount.description)
+                    RowContent(title: "Active cores", subtitle: fingerprint?.processor?.activeProcessors.description)
+                    RowContent(title: "Architecture", subtitle: fingerprint?.processor?.architecture)
+                    RowContent(title: "Kernel version", subtitle: fingerprint?.processor?.kernelVersion.description)
                 } header: {
                     Text("Processor")
                 }
                 Section {
-                    RowContent(title: "Machine", subtitle: Thumbmark.instance.fingerprint.device.machine)
-                    RowContent(title: "Model", subtitle: Thumbmark.instance.fingerprint.device.model)
-                    RowContent(title: "Host name", subtitle: Thumbmark.instance.fingerprint.device.hostName)
-                    RowContent(title: "Device name", subtitle: Thumbmark.instance.fingerprint.device.deviceName)
-                    RowContent(title: "OS release", subtitle: Thumbmark.instance.fingerprint.device.osRelease)
-                    RowContent(title: "OS version", subtitle: Thumbmark.instance.fingerprint.device.osVersion)
+                    RowContent(title: "Machine", subtitle: fingerprint?.device?.machine)
+                    RowContent(title: "Model", subtitle: fingerprint?.device?.model)
+                    RowContent(title: "Host name", subtitle: fingerprint?.device?.hostName)
+                    RowContent(title: "Device name", subtitle: fingerprint?.device?.deviceName)
+                    RowContent(title: "OS release", subtitle: fingerprint?.device?.osRelease)
+                    RowContent(title: "OS version", subtitle: fingerprint?.device?.osVersion)
                 } header: {
                     Text("Device")
                 }
                 Section {
-                    RowContent(title: "RAM", subtitle: Thumbmark.instance.fingerprint.memory.ram.description)
-                    RowContent(title: "Disk size", subtitle: Thumbmark.instance.fingerprint.memory.diskSize.description)
+                    RowContent(title: "RAM", subtitle: fingerprint?.memory?.ram.description)
+                    RowContent(title: "Disk size", subtitle: fingerprint?.memory?.diskSize.description)
                 } header: {
                     Text("Memory")
                 }
                 Section {
-                    RowContent(title: "isBoldTextEnabled", subtitle: Thumbmark.instance.fingerprint.accessibility.isBoldTextEnabled.description)
-                    RowContent(title: "isShakeToUndoEnabled", subtitle: Thumbmark.instance.fingerprint.accessibility.isShakeToUndoEnabled.description)
-                    RowContent(title: "isReduceMotionEnabled", subtitle: Thumbmark.instance.fingerprint.accessibility.isReduceMotionEnabled.description)
-                    RowContent(title: "isDarkerSystemColorsEnabled", subtitle: Thumbmark.instance.fingerprint.accessibility.isDarkerSystemColorsEnabled.description)
-                    RowContent(title: "isReduceTransparencyEnabled", subtitle: Thumbmark.instance.fingerprint.accessibility.isReduceTransparencyEnabled.description)
-                    RowContent(title: "isAssistiveTouchRunning", subtitle: Thumbmark.instance.fingerprint.accessibility.isAssistiveTouchRunning.description)
-                    RowContent(title: "preferredContentSizeCategory", subtitle: Thumbmark.instance.fingerprint.accessibility.preferredContentSizeCategory)
+                    RowContent(title: "isBoldTextEnabled", subtitle: fingerprint?.accessibility?.isBoldTextEnabled.description)
+                    RowContent(title: "isShakeToUndoEnabled", subtitle: fingerprint?.accessibility?.isShakeToUndoEnabled.description)
+                    RowContent(title: "isReduceMotionEnabled", subtitle: fingerprint?.accessibility?.isReduceMotionEnabled.description)
+                    RowContent(title: "isDarkerSystemColorsEnabled", subtitle: fingerprint?.accessibility?.isDarkerSystemColorsEnabled.description)
+                    RowContent(title: "isReduceTransparencyEnabled", subtitle: fingerprint?.accessibility?.isReduceTransparencyEnabled.description)
+                    RowContent(title: "isAssistiveTouchRunning", subtitle: fingerprint?.accessibility?.isAssistiveTouchRunning.description)
+                    RowContent(title: "preferredContentSizeCategory", subtitle: fingerprint?.accessibility?.preferredContentSizeCategory)
                 } header: {
                     Text("Accessibility")
                 }
                 Section {
-                    RowContent(title: "identifier", subtitle: Thumbmark.instance.fingerprint.locality.identifier)
-                    RowContent(title: "languageCode", subtitle: Thumbmark.instance.fingerprint.locality.languageCode)
-                    RowContent(title: "regionCode", subtitle: Thumbmark.instance.fingerprint.locality.regionCode)
-                    RowContent(title: "availableRegionCodes", subtitle: Thumbmark.instance.fingerprint.locality.availableRegionCodes.formatted())
-                    RowContent(title: "calendarIdentifier", subtitle: Thumbmark.instance.fingerprint.locality.calendarIdentifier)
-                    RowContent(title: "timezone", subtitle: Thumbmark.instance.fingerprint.locality.timezone)
-                    RowContent(title: "availableTimezones", subtitle: Thumbmark.instance.fingerprint.locality.availableTimezones.formatted())
-                    RowContent(title: "availableKeyboards", subtitle: Thumbmark.instance.fingerprint.locality.availableKeyboards.formatted())
-                    RowContent(title: "twentyFourHourTimeEnabled", subtitle: Thumbmark.instance.fingerprint.locality.twentyFourHourTimeEnabled.description)
-                    RowContent(title: "temperatureUnit", subtitle: Thumbmark.instance.fingerprint.locality.temperatureUnit)
-                    RowContent(title: "usesMetricSystem", subtitle: Thumbmark.instance.fingerprint.locality.usesMetricSystem.description)
+                    RowContent(title: "identifier", subtitle: fingerprint?.locality?.identifier)
+                    RowContent(title: "languageCode", subtitle: fingerprint?.locality?.languageCode)
+                    RowContent(title: "regionCode", subtitle: fingerprint?.locality?.regionCode)
+                    RowContent(title: "availableRegionCodes", subtitle: fingerprint?.locality?.availableRegionCodes.formatted())
+                    RowContent(title: "calendarIdentifier", subtitle: fingerprint?.locality?.calendarIdentifier)
+                    RowContent(title: "timezone", subtitle: fingerprint?.locality?.timezone)
+                    RowContent(title: "availableTimezones", subtitle: fingerprint?.locality?.availableTimezones.formatted())
+                    RowContent(title: "availableKeyboards", subtitle: fingerprint?.locality?.availableKeyboards.formatted())
+                    RowContent(title: "twentyFourHourTimeEnabled", subtitle: fingerprint?.locality?.twentyFourHourTimeEnabled.description)
+                    RowContent(title: "temperatureUnit", subtitle: fingerprint?.locality?.temperatureUnit)
+                    RowContent(title: "usesMetricSystem", subtitle: fingerprint?.locality?.usesMetricSystem.description)
                 } header: {
                     Text("Locality")
                 }
                 Section {
-                    RowContent(title: "canSendMail", subtitle: Thumbmark.instance.fingerprint.communication.canSendMail.description)
-                    RowContent(title: "canSendMesages", subtitle: Thumbmark.instance.fingerprint.communication.canSendMessages.description)
-                    RowContent(title: "canSendSubject", subtitle: Thumbmark.instance.fingerprint.communication.canSendSubject.description)
-                    RowContent(title: "canSendAttachments", subtitle: Thumbmark.instance.fingerprint.communication.canSendAttachments.description)
+                    RowContent(title: "canSendMail", subtitle: fingerprint?.communication?.canSendMail.description)
+                    RowContent(title: "canSendMesages", subtitle: fingerprint?.communication?.canSendMessages.description)
+                    RowContent(title: "canSendSubject", subtitle: fingerprint?.communication?.canSendSubject.description)
+                    RowContent(title: "canSendAttachments", subtitle: fingerprint?.communication?.canSendAttachments.description)
                 } header: {
                     Text("Communication")
                 }
             }
+        }.task {
+            id = await Thumbmark.instance.id ?? "N/A"
+            persistentId = await Thumbmark.instance.persistentId()?.uuidString ?? "N/A"
+            fingerprint = await Thumbmark.instance.fingerprint
         }
     }
 
